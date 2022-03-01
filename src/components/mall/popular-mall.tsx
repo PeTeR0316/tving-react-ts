60b17dd60ea371c6a5f96006c07f12eb8c5e358b
PeTeR0316 <paran0341@naver.com>
2022-03-01 23:12:08 +0900

    220301_상품슬라이드_기능추가

--- src/components/mall/popular-mall.tsx
+++ src/components/mall/popular-mall.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 import IMAGES from "../../assets/images";
+import ICONS from "../../assets/icons";
+
+interface ProductInfoType {
+    name:string,
+    imgSrc:string,
+    price:number,
+    discountRate:number
+
+}
 
 const PopularMallStyle = styled.div`
     width: 100%;
     height: 570px;
     background-color: #f8f6ea;
+    position: relative;
 
     .popularContainer {
         width: 1400px;
@@ -61,20 +71,124 @@ const PopularMallStyle = styled.div`
         }
 
         .goodsSlideshowArea {
-            margin-top: 50px;
-            display: flex;
-            justify-content: space-between;
+            overflow: hidden;
+            
+            .goodsSlideshowFrame {
+                margin-top: 50px;
 
-            .goodsSlideshowImg {
-                width: 256px;
-                height: 256px;
-                border-radius: 20px;
+                .goodsList {
+                    display: inline-block;
+                    width: 232px;
+                    vertical-align: top;
+
+                    :not(:nth-child(5n)) {
+                        padding-right: 60px;
+                    }
+    
+                    .goodsSlideshowImg {
+                        width: 232px;
+                        height: 232px;
+                        border-radius: 20px;
+                    }
+    
+                    .goodsName {
+                        word-break: keep-all;
+                        margin: 10px 0;
+                    }
+    
+                    .goodsPrice {
+                        font-size: 20px;
+                        font-weight: 800;
+                        margin: 0px;
+    
+                        .discountRateText {
+                            color: #ff153c;
+                            font-weight: 900;
+                            padding-right: 5px;
+                        }
+                    }
+    
+                    .discountPrice {
+                        font-size: 14px;
+                        font-weight: 400;
+                        color: #999999;
+                        text-decoration: line-through;
+                        margin: 0px;
+                    }
+    
+                }
             }
+        }
+
+        .slideNumDotted {
+            text-align: center;
+
+            .dotted{
+                display: inline-block;
+                width: 7px;
+                height: 7px;
+                border: 1px solid #000000;
+                border-radius: 7px;
+                background-color: #000000;
+                margin-right: 10px;
+            }
+
+            .currentDotted {
+                display: inline-block;
+                width: 7px;
+                height: 7px;
+                border: 1px solid #c7c7c7;
+                border-radius: 7px;
+                background-color: #ffffff;
+                margin-right: 10px;
+            }
+        }
+
+        .slideLeftBtn {
+            background-color: #f8f6ea;
+            border: none;
+            position: absolute;
+            top: 50%;
+            left: 180px;
+            transform: translateY(-50%);
+            cursor: pointer;
+        }
+    
+        .slideRightBtn {
+            background-color: #f8f6ea;
+            border: none;
+            position: absolute;
+            top: 50%;
+            right: 180px;
+            transform: translateY(-50%);
+            cursor: pointer;
         }
     }
 `
 
 const PopularMall = () => {
+    const [slideshowCount, setSlideshowCount] = useState<number>(5);
+    const [slideshowNum, setSlideshowNum] = useState<number>(0);
+    const [goodsCount, setGoodsCount] = useState<number>(0);
+
+    useEffect(() => setGoodsCount(Math.ceil(IMAGES.PRODUCT_SLIDESHOW_IMG_DUMMY.length / 5)),[]);
+
+    const dottedFn = () => {
+        let dottedEl = [];
+
+        for(let startNum = 0; startNum < goodsCount; startNum++) {
+            dottedEl.push(
+                <div className={slideshowNum === startNum ? "dotted" : "currentDotted"}
+                    onClick = {() => setSlideshowNum(startNum)}
+                    key={startNum}
+                >
+                </div>
+            );
+        }
+
+        return dottedEl;
+    }
+
     return (
         <PopularMallStyle>
             <div className="popularContainer">
@@ -86,11 +200,64 @@ const PopularMall = () => {
                 </div>
                 
                 <div className="goodsSlideshowArea">
-                    <img src={IMAGES.PRODUCT_SLIDESHOW_IMG[0]} alt="" className="goodsSlideshowImg" />
-                    <img src={IMAGES.PRODUCT_SLIDESHOW_IMG[1]} alt="" className="goodsSlideshowImg" />
-                    <img src={IMAGES.PRODUCT_SLIDESHOW_IMG[2]} alt="" className="goodsSlideshowImg" />
-                    <img src={IMAGES.PRODUCT_SLIDESHOW_IMG[3]} alt="" className="goodsSlideshowImg" />
+                    <div className="goodsSlideshowFrame"
+                        style={{
+                            width: `${slideshowCount * 100}%`, 
+                            marginLeft: `${-1400 * slideshowNum}px`,
+                            transition: `0.5s ease 0s`
+                        }}
+                    >
+                        {IMAGES.PRODUCT_SLIDESHOW_IMG_DUMMY.map((goodsInfo:ProductInfoType, index) => {
+                            return(
+                                <div className="goodsList">
+                                    <img src={goodsInfo.imgSrc} className="goodsSlideshowImg" alt="" />
+                                    <p className="goodsName">
+                                        {
+                                            goodsInfo.name.length < 33 ? goodsInfo.name :
+                                            `${goodsInfo.name.slice(0,33)}...`
+                                        }
+                                    </p>
+                                    <p className="goodsPrice">
+                                        {goodsInfo.discountRate !== 0 ? 
+                                            <span className="discountRateText">{goodsInfo.discountRate}%</span> : ""
+                                        }
+                                        {goodsInfo.price - goodsInfo.price * (goodsInfo.discountRate / 100)}
+                                    </p>
+                                    {goodsInfo.discountRate !== 0 ? 
+                                        <p className="discountPrice">{goodsInfo.price * (goodsInfo.discountRate / 100)}</p> : 
+                                        ""
+                                    }
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
+
+                <div className="slideNumDotted">
+                        {dottedFn()}
+                </div>
+
+                <button type="button" 
+                    className="slideLeftBtn"
+                    onClick={() => {
+                        if(slideshowNum !== 0) {
+                            setSlideshowNum(slideshowNum - 1);
+                        }
+                    }}
+                >
+                    <img src={ICONS.PC_ARROW_LEFT} alt="leftBtn" />
+                </button>
+
+                <button type="button" 
+                    className="slideRightBtn"
+                    onClick={() => {
+                        if(slideshowNum !== goodsCount - 1) {
+                            setSlideshowNum(slideshowNum + 1);
+                        }
+                    }}
+                >
+                    <img src={ICONS.PC_ARROW_RIGHT} alt="rightBtn" />
+                </button>
             </div>
         </PopularMallStyle>
     )
